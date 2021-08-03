638f07a789a2a9c634c93ae289bd4ebd526bea4f
ChunYu Chang <mr91217@gmail.com>
2021-08-03 16:22:38 -0400

    connect to MYSQL DATABASE

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extend: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'jiajia9567',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // app.get('/', (req, res) => {
 //     res.json({
