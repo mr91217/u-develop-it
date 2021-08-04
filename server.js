6df05abb0af682d4b34c7c22667f4b2b073f5d5a
ChunYu Chang <mr91217@gmail.com>
2021-08-03 20:03:38 -0400

    add db.query()

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
 // app.get('/', (req, res) => {
 //     res.json({
 //       message: 'Hello World'
